886ee765ca90793a30c486f48413cb0c492c4165
oussama Bourrous <oussama18bourrous@gmail.com>
2023-08-02 14:01:17 -0700

    correct

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,8 +5,15 @@ void _puts_recursion(char *s)
 {
 	if (*s == '0')
 	{
-		putchar('\n');	
-		 return;
+		putchar('\n');
+	}
+	else
+	{
+		if (*s == '.')
+			putchar('.');
+		else
+			if (*s == ',')
+				putchar(',');
 	}
 putchar(*s);
 _puts_recursion(s + 1);
