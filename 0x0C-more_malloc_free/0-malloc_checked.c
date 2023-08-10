cb52591d88e4450711acb2505b9d577a8c161c61
oussama Bourrous <oussama18bourrous@gmail.com>
2023-08-10 16:53:18 -0700

    task 1

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,7 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
-
+/**
+ * malloc_checked - function to request memory
+ * @b: the element that malloc request to
+ *
+ * Return: NULL
+ */
 void *malloc_checked(unsigned int b)
 {
 	void *uid;
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * string_nconcat - function to concate two string
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes in second string
+ *
+ * Return: NULL
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *arr, *x;
+	unsigned int i, a1, a2;
+
+	a1 = strlen(s1);
+	a2 = strlen(s2);
+
+	arr = malloc((sizeof(char) * a1) + (sizeof(char) * n));
+		if (arr == NULL)
+			return (NULL);
+
+	x = malloc(sizeof(char) * n);
+	for (i = 0 ; i < n ; i++)
+		x[i] = s2[i];
+
+
+	strcpy(arr, s1);
+	if (n >= a2)
+		strcat(arr, s2);
+	else
+		strcat(arr, x);
+
+	return (arr);
+}
