56bc78a934e27c136023f5c2f8d311782e3a3f71
oussama Bourrous <oussama18bourrous@gmail.com>
2023-08-11 05:32:49 -0700

    task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	memset(ptr, 0, nmemb * size);
+
+	return (ptr);
+}
