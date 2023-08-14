04dcca69829013844b94de81fc45adf2de450a4f
oussama Bourrous <oussama18bourrous@gmail.com>
2023-08-14 13:51:53 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog - to define a dog
+ * @name: the name of dog
+ * @owner: the name of the person
+ * @age: the age of dog
+ *
+ * we are define a primission
+ * for this dog
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
