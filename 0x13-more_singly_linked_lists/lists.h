16a2296cf654ed5264c748bcb21277da2baaef86
oussama Bourrous <oussama18bourrous@gmail.com>
2023-08-29 04:12:59 -0700

    task 0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -13,6 +13,8 @@ typedef struct listint_s
     int n;
     struct listint_s *next;
 } listint_t;
+#include <stdio.h>
+#include <stdlib.h>
 size_t print_listint(const listint_t *h);
 char _putchar(char c);
 
