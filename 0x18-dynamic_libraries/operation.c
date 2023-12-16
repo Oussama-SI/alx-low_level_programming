492eabe1e2ca163f4bb35cda7b304e0c73bac183
oussama BOURROUS <oussama18bourrous@gmail.com>
2023-12-15 17:02:08 -0800

    task 100

---
+++ 0x18-dynamic_libraries/operation.c
@@ -0,0 +1,51 @@
+#include "mmain.h"
+/*
+ * add - operation declared in py
+ * @a: first operator
+ * @b: second operator
+ * Return: the result integer
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/*
+ * sub - operation declared in py
+ * @a: first operator
+ * @b: second operator
+ * Return: the result integer
+ */
+int sub(int a, int b)
+{
+        return (a - b);
+}
+/*
+ * mul - operation declared in py
+ * @a: first operator
+ * @b: second operator
+ * Return: the result integer
+ */
+int mul(int a, int b)
+{
+        return (a * b);
+}
+/*
+ * div - operation declared in py
+ * @a: first operator
+ * @b: second operator
+ * Return: the result integer
+ */
+int div(int a, int b)
+{
+        return (a / b);
+}
+/*
+ * mod - operation declared in py
+ * @a: first operator
+ * @b: second operator
+ * Return: the result integer
+ */
+int mod(int a, int b)
+{
+        return (a % b);
+}
